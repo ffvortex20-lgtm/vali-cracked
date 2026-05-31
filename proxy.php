29249c4bbabba03c113e7fe3695fb24400171aee
vortex <ffvortex20@gmail.com>
2026-05-31 10:47:30 -0300

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,7 +1,14 @@
 <?php
+// proxy.php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
 $url = 'https://viku.mypdftools.site/api/generate';
+
+// Pega os dados que o seu site enviou
 $data = file_get_contents('php://input');
 
+// Faz a requisição para a API real
 $options = [
     'http' => [
         'header'  => "Content-type: application/json\r\n",
@@ -9,6 +16,10 @@ $options = [
         'content' => $data,
     ],
 ];
+
 $context  = stream_context_create($options);
-echo file_get_contents($url, false, $context);
+$result = file_get_contents($url, false, $context);
+
+// Devolve a resposta para o seu site
+echo $result;
 ?>
